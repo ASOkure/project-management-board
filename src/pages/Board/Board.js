a0e82cae077a6b72dffab28245b0fcb06387ff80
Akanimo Okure <ao475@cam.ac.uk>
2023-08-01 23:01:15 +0100

    refactor and use custom hook to fetch data

--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -1,5 +1,5 @@
-import {useState, useEffect} from 'react';
-
+import useDataFetching from 
+'../../hooks/useDataFetching';
 import Lane from '../../components/Lane/Lane';
 import './Board.css';
 
@@ -11,30 +11,9 @@ const lanes = [
 ];
 
 function Board() {
-  const [loading, setLoading] = useState(false);
-  const [tasks, setTasks] = useState([]);
-  const [error, setError] = useState('');
+  
+  const [loading, error, tasks]= useDataFetching ('https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks');
 
-  useEffect ( ()=> {
-   async function fetchData() {
-    try { 
-      const  tasks = await fetch (
-
-        'https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks'
-      );
-
-     const result = await tasks.json();
-             if (result) {
-                 setTasks(result);
-                 setLoading(false);
-               }
-             } catch (e) {
-               setLoading(false);
-               setError(e.message);
-             }
-           }
-           fetchData();
-         }, []);
 
   return (
     <div className='Board-wrapper'>
